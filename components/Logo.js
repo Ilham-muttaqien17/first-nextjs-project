c207f58bb21c1eb190c32fef5ef90e938514c122
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-08-03 23:13:32 +0700

    test

--- components/Logo.js
+++ components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo() {
     return (
         <div className="uppercase text-2xl font-semibold tracking-widest text-white">
-            ILHAM
+            ILHAM M
         </div>
     )
 }
